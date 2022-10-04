dfd352fdba742df7e39f09d8a96856a453e068d4
Kewin Titus <kewintitus@gmail.com>
2022-10-04 19:57:25 +0530

    [FEAT] - added slice for managing compose state

--- src/features/mailSlice.js
+++ src/features/mailSlice.js
@@ -20,20 +20,16 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+export const mailSlice = createSlice({
+  name: 'mail',
+  initialState: { sendMessageIsOpen: false },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    openSendMessage: (state) => {
+      state.sendMessageIsOpen = true;
     },
-    decrement: (state) => {
-      state.value -= 1;
+    closeSendMessage: (state) => {
+      state.sendMessageIsOpen = false;
     },
     // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action) => {
@@ -54,20 +50,20 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { openSendMessage, closeSendMessage } = mailSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectSendMessageIsOpen = (state) => state.mail.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
+// export const incrementIfOdd = (amount) => (dispatch, getState) => {
+//   const currentValue = selectCount(getState());
+//   if (currentValue % 2 === 1) {
+//     dispatch(incrementByAmount(amount));
+//   }
+// };
 
-export default counterSlice.reducer;
+export default mailSlice.reducer;
