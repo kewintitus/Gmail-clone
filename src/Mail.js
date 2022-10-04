d00bc1610d01dac32fe86e05979a4d44fb868fc3
Kewin Titus <kewintitus@gmail.com>
2022-10-04 18:33:32 +0530

    [FEAT] - UI: Mail message description UI completed

--- src/Mail.js
+++ src/Mail.js
@@ -12,9 +12,16 @@ import LabelOutlinedIcon from '@mui/icons-material/LabelOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import PrintOutlinedIcon from '@mui/icons-material/PrintOutlined';
+import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined';
+import UserImg from '../src/images/userIcon.png';
 import { IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
+import { fontSize } from '@mui/system';
 const Mail = () => {
   const navigate = useNavigate();
 
@@ -22,49 +29,106 @@ const Mail = () => {
     navigate('/');
   };
 
+  const mailIconToolsStyle = {
+    color: '#202124',
+    fontSize: '20px',
+  };
+
   return (
     <div className="mail">
       <div className="mail-tools">
         <div className="mail-toolsLeft">
           <IconButton onClick={backButtonHandler}>
-            <ArrowBackIcon />
+            <ArrowBackIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <ArchiveOutlinedIcon />
+            <ArchiveOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <ReportGmailerrorredOutlinedIcon />
+            <ReportGmailerrorredOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <DeleteOutlinedIcon style={{ color: '#000' }} />
+            <DeleteOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <MarkunreadOutlinedIcon />
+            <MarkunreadOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <AccessTimeOutlinedIcon />
+            <AccessTimeOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <AddTaskOutlinedIcon />
+            <AddTaskOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <DriveFileMoveOutlinedIcon />
+            <DriveFileMoveOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <LabelOutlinedIcon />
+            <LabelOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <MoreVertOutlinedIcon />
+            <MoreVertOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
         </div>
         <div className="mail-toolsRight">
           <IconButton>
-            <ChevronLeftOutlinedIcon />
+            <ChevronLeftOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
           <IconButton>
-            <ChevronRightOutlinedIcon />
+            <ChevronRightOutlinedIcon style={mailIconToolsStyle} />
           </IconButton>
         </div>
+      </div>
+      <div className="mail-body">
+        <div className="mailSubject">
+          <div className="mail-bodySubject">
+            <h2>Subject</h2>
+
+            <div className="mail-bodySubject--label">
+              <div>Inbox</div>
+              <CloseOutlinedIcon style={{ fontSize: '12px' }} />
+            </div>
+          </div>
+          <div className="mail-bodySubject--actions">
+            <IconButton>
+              <PrintOutlinedIcon />
+            </IconButton>
+            <IconButton>
+              <OpenInNewOutlinedIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className="mail-bodyUser">
+          <div className="mail-bodyUser--details">
+            <div className="userDetails">
+              <div className="userImg">
+                <img src={UserImg}></img>
+              </div>
+              <div>
+                <div className="userDetailsInfo">
+                  <h4>UserName</h4>
+                  <p>{'<emailId>'}</p>
+                </div>
+                <div className="userDetailsActions">
+                  <p>To me</p>
+                  <ArrowDropDownOutlinedIcon />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mail-bodyUser--actions">
+            <span>today</span>
+            <IconButton>
+              <StarBorderOutlinedIcon />
+            </IconButton>
+            <IconButton>
+              <ReplyOutlinedIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertOutlinedIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className="mail-bodyContent"></div>
       </div>
     </div>
   );
