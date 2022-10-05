1c110fcfd7c2e38a4ad2f6fd64965429ed0144b4
Kewin Titus <kewintitus@gmail.com>
2022-10-05 16:18:04 +0530

    [FEAT] - firebase emails collection connentio established

--- src/SendMail.js
+++ src/SendMail.js
@@ -8,7 +8,26 @@ import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { closeSendMessage } from './features/mailSlice';
 import { db } from './firebase';
+import { Timestamp } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
+import { addDoc, collection } from 'firebase/firestore';
+
+const addData = async (formData) => {
+  try {
+    const docRef = await addDoc(collection(db, 'emails'), {
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timestamp: Timestamp.now(),
+    });
+    console.log('Document written with ID: ', docRef.id);
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+};
+
 const SendMail = () => {
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -18,9 +37,21 @@ const SendMail = () => {
 
   const onSubmit = (formData) => {
     console.log(formData);
-    db.collection('emails').add();
+    addData(formData);
+    // db.collection('emails').add({
+    //   to: formData.to,
+    //   subject: formData.subject,
+    //   message: formData.message,
+    //   timestamp: Timestamp.now(),
+    // });
+    // addDoc(emailRef, {
+    //   to: formData.to,
+    //   subject: formData.subject,
+    //   message: formData.message,
+    //   timestamp: Timestamp.now(),
+    // });
+    dispatch(closeSendMessage());
   };
-  const dispatch = useDispatch();
   const closeSendMessageHandler = () => {
     dispatch(closeSendMessage());
   };
@@ -36,14 +67,14 @@ const SendMail = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="To"
-          type="text"
+          type="email"
           {...register('to', { required: true })}
         />
         {errors.to && <p className="sendMailError">Invalid</p>}
 
         <input
           placeholder="Subject"
-          type="email"
+          type="text"
           {...register('subject', { required: true })}
         />
         {errors.subject && <p className="sendMailError">Invalid</p>}
